7feed74b8f788aca1e69cbaf6a5f1213cead749e
Syuichi Murashima <s.murashima@gloops.com>
2017-10-30 16:16:30 +0900

    1st commit

---
+++ AES256.ps1
@@ -0,0 +1,205 @@
+﻿Param($Path, $KeyPath, [switch]$Encrypto, [switch]$Decrypto)
+
+# 暗号ファイルの拡張子
+$ExtName = "aes256"
+
+#####################################################################
+# 文字列をバイト配列にする
+#####################################################################
+function String2Byte( $String ){
+	$Byte = [System.Text.Encoding]::UTF8.GetBytes($String)
+	return $Byte
+}
+
+#####################################################################
+# バイト配列を文字列にする
+#####################################################################
+function Byte2String( $Byte ){
+	$String = [System.Text.Encoding]::UTF8.GetString($Byte)
+	return $String
+}
+
+#####################################################################
+# Base64 をバイト配列にする
+#####################################################################
+function Base642Byte( $Base64 ){
+	$Byte = [System.Convert]::FromBase64String($Base64)
+	return $Byte
+}
+
+#####################################################################
+# バイト配列を Base64 にする
+#####################################################################
+function Byte2Base64( $Byte ){
+	$Base64 = [System.Convert]::ToBase64String($Byte)
+	return $Base64
+}
+
+##################################################
+# AES 暗号化
+##################################################
+function AESEncrypto($KeyByte, $PlainByte){
+	$KeySize = 256
+	$BlockSize = 128
+	$Mode = "CBC"
+	$Padding = "PKCS7"
+
+	# アセンブリロード
+	Add-Type -AssemblyName System.Security
+
+	# AES オブジェクトの生成
+	$AES = New-Object System.Security.Cryptography.AesCryptoServiceProvider
+
+	# 各値セット
+	$AES.KeySize = $KeySize
+	$AES.BlockSize = $BlockSize
+	$AES.Mode = $Mode
+	$AES.Padding = $Padding
+
+	# IV 生成
+	$AES.GenerateIV()
+
+	# 生成した IV
+	$IV = $AES.IV
+
+	# 鍵セット
+	$AES.Key = $KeyByte
+
+	# 暗号化オブジェクト生成
+	$Encryptor = $AES.CreateEncryptor()
+
+	# 暗号化
+	$EncryptoByte = $Encryptor.TransformFinalBlock($PlainByte, 0, $PlainByte.Length)
+
+	# IV と暗号化した文字列を結合
+	$DataByte = $IV + $EncryptoByte
+
+	# オブジェクト削除
+	$Encryptor.Dispose()
+	$AES.Dispose()
+
+	return $DataByte
+}
+
+
+##################################################
+# AES 復号化
+##################################################
+function AESDecrypto($ByteKey, $ByteString){
+	$KeySize = 256
+	$BlockSize = 128
+	$IVSize = $BlockSize / 8
+	$Mode = "CBC"
+	$Padding = "PKCS7"
+
+	# IV を取り出す
+	$IV = @()
+	for( $i = 0; $i -lt $IVSize; $i++){
+		$IV += $ByteString[$i]
+	}
+
+	# アセンブリロード
+	Add-Type -AssemblyName System.Security
+
+	# オブジェクトの生成
+	$AES = New-Object System.Security.Cryptography.AesCryptoServiceProvider
+
+	# 各値セット
+	$AES.KeySize = $KeySize
+	$AES.BlockSize = $BlockSize
+	$AES.Mode = $Mode
+	$AES.Padding = $Padding
+
+	# IV セット
+	$AES.IV = $IV
+
+	# 鍵セット
+	$AES.Key = $ByteKey
+
+	# 復号化オブジェクト生成
+	$Decryptor = $AES.CreateDecryptor()
+
+	try{
+		# 復号化
+		$DecryptoByte = $Decryptor.TransformFinalBlock($ByteString, $IVSize, $ByteString.Length - $IVSize)
+	}
+	catch{
+		$DecryptoByte = $null
+	}
+
+	# オブジェクト削除
+	$Decryptor.Dispose()
+	$AES.Dispose()
+
+	return $DecryptoByte
+}
+
+##################################################
+# main
+##################################################
+
+if( ($Path -eq $null) -or ($KeyPath -eq $null)){
+	echo "Usage..."
+	echo " .\aes256.ps1 [-Encrypto|-Decrypto] -Path InputFile -KeyPath KeyFile"
+	exit
+}
+
+# Data
+if( -not (Test-Path $Path )){
+	echo "[FAIL] $Path not found."
+	exit
+}
+
+
+if( -not (Test-Path $KeyPath )){
+	echo "[FAIL] $KeyPath not found."
+	exit
+}
+
+if( (($Encrypto -eq $fals) -and ( $Decrypto -eq $false)) -or(($Encrypto -eq $true) -and ( $Decrypto -eq $true))){
+	echo "[FAIL] select -Encrypto or -Decrypto"
+	exit
+}
+
+# 鍵読み込み
+$Base64Key = Get-Content $KeyPath
+$ByteKey = Base642Byte $Base64Key
+
+# 暗号化
+if( $Encrypto ){
+
+	# 暗号化ファイル名
+	$EncryptoFileName = $Path + "." + $ExtName
+
+	# データファイル読み込み
+	$BytePlainData = Get-Content $Path -Encoding Byte
+
+	# 暗号
+	$ByteEncryptoData = AESEncrypto $ByteKey $BytePlainData
+	$Base64EncryptoData = Byte2Base64 $ByteEncryptoData
+
+	# ファイル出力
+	Set-Content -Path $EncryptoFileName -Value $Base64EncryptoData -Encoding UTF8
+}
+# 復号化
+else{
+	# 拡張子確認
+	if( $Path -notmatch $ExtName ){
+		echo "[FAIL] $Path は暗号化ファイルではない"
+		exit
+	}
+
+	# 復号ファイル名
+	$ChangeString = "."+ $ExtName
+	$DecryptoFileName = $Path.Replace($ChangeString,"")
+
+	# 暗号ファイル読み込み
+	$Base64EncryptoData = Get-Content $Path
+
+	# 復号
+    $ByteEncryptoData = Base642Byte $Base64EncryptoData
+	$BytePlainData = AESDecrypto $ByteKey $ByteEncryptoData
+
+	# 平文ファイル出力
+	Set-Content -Path $DecryptoFileName -Value $BytePlainData -Encoding Byte
+}
---
+++ Make256Key.ps1
@@ -0,0 +1,86 @@
+﻿Param($Path)
+
+#####################################################################
+# 文字列をバイト配列にする
+#####################################################################
+function String2Byte( $String ){
+	$Byte = [System.Text.Encoding]::UTF8.GetBytes($String)
+	return $Byte
+}
+
+#####################################################################
+# バイト配列を文字列にする
+#####################################################################
+function Byte2String( $Byte ){
+	$String = [System.Text.Encoding]::UTF8.GetString($Byte)
+	return $String
+}
+
+#####################################################################
+# Base64 をバイト配列にする
+#####################################################################
+function Base642Byte( $Base64 ){
+	$Byte = [System.Convert]::FromBase64String($Base64)
+	return $Byte
+}
+
+#####################################################################
+# バイト配列を Base64 にする
+#####################################################################
+function Byte2Base64( $Byte ){
+	$Base64 = [System.Convert]::ToBase64String($Byte)
+	return $Base64
+}
+
+
+
+##################################################
+# セッション鍵生成
+##################################################
+function CreateRandomKey( $BitSize ){
+    if( ($BitSize % 8) -ne 0 ){
+        echo "Key size Error"
+        return $null
+    }
+    # アセンブリロード
+    Add-Type -AssemblyName System.Security
+
+    # バイト数にする
+    $ByteSize = $BitSize / 8
+
+    # 入れ物作成
+    $KeyBytes = New-Object byte[] $ByteSize
+
+    # オブジェクト 作成
+    $RNG = New-Object System.Security.Cryptography.RNGCryptoServiceProvider
+
+    # 鍵サイズ分の乱数を生成
+    $RNG.GetNonZeroBytes($KeyBytes)
+
+    # オブジェクト削除
+    $RNG.Dispose()
+
+    return $KeyBytes
+}
+
+
+##################################################
+# main
+##################################################
+
+if( $Path -eq $null ){
+	echo "Usage..."
+	echo " .\make256key.ps1 -Path KeyFile"
+	exit
+}
+
+
+# 256 バイトの共通鍵生成
+$ByteKey = CreateRandomKey 256
+
+
+# Base 64 にする
+$Base64Key = Byte2Base64 $ByteKey
+
+# ファイル出力
+Set-Content -Path $Path -Value $Base64Key -Encoding UTF8
